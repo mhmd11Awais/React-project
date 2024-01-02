0840686b711e773d24a8eddb9fbada4757e64aa8
Muhammad Awais <118798522+mhmd11Awais@users.noreply.github.com>
2024-01-02 12:22:42 +0500

    Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className="d-flex align-items-center justify-content-center pb-5">
           <div className="col-md-6">
             <p className="mb-3 mb-md-0"> {" "}
-              <a  href="https://sahibsingh.dev" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Muhammad Awais</a>
+              <a  href="https://www.linkedin.com/in/muhammad-awais-550aa7241/" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Muhammad Awais</a>
             </p>
             <a className="text-dark fs-4" href="https://github.com/mhmd11Awais" target="_blank" rel="noreferrer">
               <i className="fa fa-github"></i>
